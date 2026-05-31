a14b1356efe46ba12f459a66d12a20d8cc226a5b
Tanmaya Harichandan <tanmaya0102@gmail.com>
2026-05-31 21:47:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,9 @@ if ingredients_list:
       st.subheader(fruit_chosen + ' Nutrition Information')
       smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
       sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-      my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','"""+ name_on_order+ """')"""
-      time_to_insert = st.button('Submit Order')
+    time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+name_on_order+"!", icon="✅")
