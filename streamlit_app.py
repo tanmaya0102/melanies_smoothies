320c365ef33f19a7908f8fd6f63e38a557255f9b
Tanmaya Harichandan <tanmaya0102@gmail.com>
2026-05-31 21:25:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,15 +17,15 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list =st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 if ingredients_list:
     ingredients_string  = ''
     for fruit_chosen in ingredients_list:
-        ingredients_string+=fruit_chosen + ' '
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+      ingredients_string+=fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+      sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+      my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','"""+ name_on_order+ """')"""
-    time_to_insert = st.button('Submit Order')
+      time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+name_on_order+"!", icon="✅")
