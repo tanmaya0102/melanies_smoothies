f305337ece356c5531a8298fb30a165392e14fac
Tanmaya Harichandan <tanmaya0102@gmail.com>
2026-05-31 21:21:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list =st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
 if ingredients_list:
     ingredients_string  = ''
